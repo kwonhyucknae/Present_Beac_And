e413c503e3d5c8cd86a50faeabcff93b25105876
kwonhyucknae <nhk321@naver.com>
2017-05-13 14:56:22 +0900

    비콘 송수신 확인

--- app/src/main/java/com/androidtown/beacon_estimote_attend/Fragment/FragmentA.java
+++ app/src/main/java/com/androidtown/beacon_estimote_attend/Fragment/FragmentA.java
@@ -2,7 +2,6 @@ package com.androidtown.beacon_estimote_attend.Fragment;
 
 import android.app.Fragment;
 import android.content.SharedPreferences;
-import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -16,12 +15,19 @@ import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
 import com.androidtown.beacon_estimote_attend.Framework.MainAjax;
 import com.androidtown.beacon_estimote_attend.R;
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
 
 import org.json.JSONArray;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.UUID;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -33,6 +39,11 @@ import static android.content.Context.MODE_PRIVATE;
 public class FragmentA extends Fragment{
 
     private static String sLog="Time_Table";
+    private BeaconManager beaconManager;
+    private Region region;
+
+    private TextView bcte;
+
     public String Yoil;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -47,66 +58,24 @@ public class FragmentA extends Fragment{
 
         SharedPreferences prefs = this.getActivity().getSharedPreferences("Login",MODE_PRIVATE);
         String strid=prefs.getString("id","");
-/*
-        long now=System.currentTimeMillis();
 
-        Date date=new Date(now);
-        // 시간 포맷 지정
+        bcte=(TextView)getView().findViewById(R.id.bctest);
 
-        SimpleDateFormat CurDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
-        SimpleDateFormat CurTimeFormat = new SimpleDateFormat("HH시 mm분");
-        SimpleDateFormat CurYearFormat = new SimpleDateFormat("yyyy");
-        SimpleDateFormat CurMonthFormat = new SimpleDateFormat("MM");
-        SimpleDateFormat CurDayFormat = new SimpleDateFormat("dd");
-        SimpleDateFormat CurHourFormat = new SimpleDateFormat("HH");
-        SimpleDateFormat CurMinuteFormat = new SimpleDateFormat("mm");
+        beaconManager=new BeaconManager(this.getActivity());
 
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                if(!list.isEmpty())
+                {
+                    Beacon nearestBeacon=list.get(0);
+                    Log.d("Airport","Nearest places: "+nearestBeacon.getRssi());
+                    bcte.setText(nearestBeacon.getRssi()+"");
+                }
+            }
+        });
+        region=new Region("ranged region", UUID.fromString("E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"),40001,10562);
 
-// 지정된 포맷으로 String 타입 리턴
-        String strCurDate = CurDateFormat.format(date);
-        String strCurTime = CurTimeFormat.format(date);
-        String strCurYear = CurYearFormat.format(date);
-        String strCurMonth = CurMonthFormat.format(date);
-        String strCurDay = CurDayFormat.format(date);
-        String strCurHour = CurHourFormat.format(date);
-        String strCurMinute = CurMinuteFormat.format(date);
-
-        String Yoil;
-
-        Calendar cal=Calendar.getInstance();
-        int year=Integer.parseInt(strCurYear);
-        int month=Integer.parseInt(strCurMonth);
-        int datt=Integer.parseInt(strCurDay);
-
-        cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month);
-        cal.set(Calendar.DATE, datt);
-
-
-        switch (cal.get(Calendar.DAY_OF_WEEK)){
-            case 1:
-                Yoil="SUN";
-                break;
-            case 2:
-                Yoil="Mon";
-                break;
-            case 3:
-                Yoil="Tue";
-                break;
-            case 4:
-                Yoil="Wed";
-                break;
-            case 5:
-                Yoil="Thu";
-                break;
-            case 6:
-                Yoil="Fri";
-                break;
-            case 7:
-                Yoil="Sat";
-                break;
-        }
-*/
         ajax.getTimeTable(strid,new AjaxCallback<JSONArray>()
         {
             @Override
@@ -116,11 +85,12 @@ public class FragmentA extends Fragment{
                 if(object!=null)
                 {
                     try {
+
                         int leng=object.length();
                         TextView nowcourse=(TextView)getView().findViewById(R.id.NowCourseTx);
                         for(int i=0;i<leng;i++) {
 
-                            long now=System.currentTimeMillis();
+                           long now=System.currentTimeMillis();
                             now=now+32400000;
                             Date date=new Date(now);
                             // 시간 포맷 지정
@@ -179,6 +149,7 @@ public class FragmentA extends Fragment{
                                     break;
                             }
 
+
                             String test = object.getJSONObject(i).get("Course_Name").toString();
                             String sta = object.getJSONObject(i).get("Start_Time").toString();
                             String end = object.getJSONObject(i).get("End_Time").toString();
@@ -208,6 +179,20 @@ public class FragmentA extends Fragment{
                     //monday[0].setText("object가 null이야");
                     //monday[0].setBackgroundColor(Color.LTGRAY);
                 }
+            }
+        });
+    }
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this.getActivity());
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
             }
         });
     }
